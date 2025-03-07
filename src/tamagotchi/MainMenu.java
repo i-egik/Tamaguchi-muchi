0dfcd1f1351f64106fb3c72bcc8ee7a622a4f85f
i-egik <i-egik@mail.ru>
2025-03-07 23:51:19 +0300

    1

---
+++ src/tamagotchi/MainMenu.java
@@ -0,0 +1,15 @@
+package tamagotchi;
+
+import java.util.Scanner;
+
+public class MainMenu {
+    public void start(){
+        System.out.println("Время яиц!!!!");
+        Scanner input = new Scanner(System.in);
+        String inputer = input.nextLine().trim().toLowerCase();
+        while (!"exit".equals(inputer)){
+
+        }
+    }
+
+}
