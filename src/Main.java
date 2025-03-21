75c96a8073c562ed0185f3c678d73a3c319325bc
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-03-21 23:49:10 +0300

    4 stage

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import tamagotchi.GameCircle;
 import tamagotchi.MainMenu;
 import tamagotchi.Tamagotchi;
 
 public class Main {
     public static void main(String[] args) {
         Tamagotchi tamagotchi = new Tamagotchi();
+        GameCircle circle = new GameCircle(tamagotchi);
         MainMenu mainMenu = new MainMenu();
+        circle.start();
         mainMenu.start(tamagotchi, tamagotchi);
     }
 }
--- src/tamagotchi/GameCircle.java
+++ src/tamagotchi/GameCircle.java
@@ -2,19 +2,36 @@ package tamagotchi;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GameCircle {
     private final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
     private final Ticker ticker;
+    private final AtomicBoolean running = new AtomicBoolean(false);
 
     public GameCircle(Ticker ticker) {
         this.ticker = ticker;
+        AtomicInteger counter = new AtomicInteger(0);
+        service.scheduleWithFixedDelay(() -> {
+            if (running.get()) {
+                ticker.tickSecond();
+                counter.incrementAndGet();
+                if (counter.intValue() == 5 * 60) {
+                    ticker.tick5Mins();
+                    counter.set(0);
+                }
+            }
+        }, 1, 1, TimeUnit.SECONDS);
     }
 
-    public void start() {//TODO
+    public void start() {
+        running.set(true);
     }
 
-    public void pouse() {//TODO
+    public void pause() {
+        running.set(false);
     }
 
     public interface Ticker {
